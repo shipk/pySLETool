3f5e8e3df34c1f53710f553c459f7f4043d78235
Konstantin Shipkov <shipkovk@gmail.com>
2017-10-11 11:29:41 +0300

    First ver

---
+++ pySLETool.py
@@ -0,0 +1,50 @@
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+from tkinter.messagebox import *
+
+class StatusBar(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self._lblState = Label(self, text='', bd=2, relief=SUNKEN, width=15, anchor = W)
+        self._lblState.pack(side=LEFT)
+
+class SLETool(Frame):
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent)
+        self.pack(side=TOP, fill=BOTH, expand=YES)
+        self.iniCtrls()
+        self.bind("<Destroy>", self.onDestroy)
+    def iniCtrls(self):
+        frmTop = Frame(self)
+        frmTop.pack(side=TOP, fill=X)
+        
+        self.status_bar = StatusBar(self)
+        self.status_bar.pack(side=BOTTOM, fill=X)
+        self.status_bar.config(bd=2, relief=SUNKEN)
+
+        frmMiddle = Frame(self)
+        frmMiddle.pack(fill=BOTH, expand=YES)
+
+        self.btnCommaCount = Button(frmTop, text='Comma count',  command=self.onCommaCount)
+        self.btnCommaCount.pack(side=LEFT, padx=5, pady=5)
+
+        Button(frmTop, text='Clear',  command=self.onClear).pack(side=RIGHT, padx=5, pady=5)
+        
+        self.st = ScrolledText(frmMiddle, font=('courier', 9, 'normal'))
+        self.st.pack(side=TOP, fill=BOTH, expand=YES)
+    def onCommaCount(self):
+        s = self.st.get('1.0', END)
+        cnt = 0
+        for c in s:
+            if c == ',':
+                cnt = cnt + 1
+        showinfo("Comma count", 'Comma count is ' + str(cnt))
+    def onClear(self):
+        self.st.delete('1.0', END)
+    def onDestroy(self, event):
+        pass
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title("SLETool")
+    SLETool(root).mainloop()
