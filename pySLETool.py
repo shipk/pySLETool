b5b239b5b762fae443ef18bbc3dfd983b8d567d3
Konstantin Shipkov <shipkovk@gmail.com>
2017-10-13 10:38:26 +0300

    .

--- pySLETool.py
+++ pySLETool.py
@@ -29,8 +29,14 @@ class SLETool(Frame):
         self.btnCommaCount = Button(frmTop, text='Comma count',  command=self.onCommaCount)
         self.btnCommaCount.pack(side=LEFT, padx=5, pady=5)
 
-        self.btnMultiply = Button(frmTop, text='Multiply',  command=self.onMultiply)
+        self.btnMultiply = Button(frmTop, text='Multiply from 48',  command=self.onMultiply)
         self.btnMultiply.pack(side=LEFT, padx=5, pady=5)
+
+        self.btnMultiply1 = Button(frmTop, text='Multiply1',  command=self.onMultiply1)
+        self.btnMultiply1.pack(side=LEFT, padx=5, pady=5)
+
+        self.btnBlock = Button(frmTop, text='Block from 1..48 to 49..96',  command=self.onBlock)
+        self.btnBlock.pack(side=LEFT, padx=5, pady=5)
 
         Button(frmTop, text='Clear',  command=self.onClear).pack(side=RIGHT, padx=5, pady=5)
         
@@ -49,6 +55,44 @@ class SLETool(Frame):
         for i in range(49,96+1):
             sa = re.sub('48', str(i), s)
             s1 = s1 + sa
+        self.st.insert(END, '===================================================\n')
+        self.st.insert(END, s1)
+    def onMultiply1(self):
+        s = self.st.get('1.0', END+'-1c')
+        s1 = ""
+        for i in range(2,48+1):
+            sa = re.sub('1', str(i), s)
+            s1 = s1 + sa
+        self.st.insert(END, '===================================================\n')
+        self.st.insert(END, s1)
+    def onBlock(self):
+        s = self.st.get('1.0', END+'-1c')
+        s1 = s
+        for i in range(1,48+1):
+            # "xx,"
+            ma = re.compile(r'(.*)([^0-9])('+str(i)+',)(.*)', re.DOTALL)
+            while True:
+                mo = ma.match(s1)
+                if mo is None: break
+                g = mo.groups()
+                #print('s1=%s groups 0=%s 1=%s 2=%s 3=%s' % (s1, g[0], g[1], g[2], g[3]))
+                s1 = g[0] + g[1] + str(i+48) + ',' + g[3]
+            # "xx "
+            ma = re.compile(r'(.*)([^0-9])('+str(i)+' )(.*)', re.DOTALL)
+            while True:
+                mo = ma.match(s1)
+                if mo is None: break
+                g = mo.groups()
+                #print('s1=%s groups 0=%s 1=%s 2=%s 3=%s' % (s1, g[0], g[1], g[2], g[3]))
+                s1 = g[0] + g[1] + str(i+48) + ' ' + g[3]
+            # "xx)"
+            ma = re.compile(r'(.*)([^0-9])('+str(i)+'\))(.*)', re.DOTALL)
+            while True:
+                mo = ma.match(s1)
+                if mo is None: break
+                g = mo.groups()
+                #print('s1=%s groups 0=%s 1=%s 2=%s 3=%s' % (s1, g[0], g[1], g[2], g[3]))
+                s1 = g[0] + g[1] + str(i+48) + ')' + g[3]
         self.st.insert(END, '===================================================\n')
         self.st.insert(END, s1)
     def onClear(self):
